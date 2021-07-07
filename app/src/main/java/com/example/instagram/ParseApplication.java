f3628ccfad8513712aad4fcf2334380d46d5d206
Nicole Liang <nicolenliang@gmail.com>
2021-07-07 17:48:14 -0400

    created ParseObject class Post

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application
 {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // must register Parse model before initializing
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("4DC4JknX6yi14XsA676zgFvRSvJ8t4DWOTpRZuzP")
