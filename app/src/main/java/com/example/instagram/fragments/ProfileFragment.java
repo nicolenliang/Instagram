ab34a13c94743c4abd9daf850cd2b1f0389945fe
Nicole Liang <nicolenliang@gmail.com>
2021-07-09 02:15:37 -0400

    implement grid layout for profile view

--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,6 +1,14 @@
 package com.example.instagram.fragments;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.instagram.Post;
 import com.parse.FindCallback;
@@ -12,10 +20,35 @@ import java.util.List;
 
 public class ProfileFragment extends PostsFragment
 {
+    private RecyclerView rvPosts;
+    private SwipeRefreshLayout swipeContainer;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.rvPosts = super.rvPosts;
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 3);
+        rvPosts.setAdapter(postsAdapter);
+        rvPosts.setLayoutManager(gridLayoutManager);
+
+        this.swipeContainer = super.swipeContainer;
+        this.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                postsAdapter.clear();
+                queryPosts();
+                swipeContainer.setRefreshing(false); // signals that refreshing has finished
+            }
+        });
+    }
+
     @Override
     protected void queryPosts()
     {
-        super.queryPosts();
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class); // specify which class to query
         query.include(Post.KEY_USER); // specify object ID
         query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser()); // filters posts by current user
@@ -33,6 +66,7 @@ public class ProfileFragment extends PostsFragment
                 }
                 for (Post post : objects)
                     Log.i(TAG, "Post: " + post.getDescription() + "; Username: " + post.getUser().getUsername());
+                allPosts.clear();
                 allPosts.addAll(objects);
                 postsAdapter.notifyDataSetChanged();
             }
